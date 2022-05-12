c244dd737a384e7656941c2c303a28fd1e307eb2
sklad <javajetbrains13rus@gmail.com>
2022-05-12 17:29:18 +0300

    add

---
+++ hyperskill/coffeemachine/main.js
@@ -0,0 +1,9 @@
+const input = require('prompt-sync');
+
+console.log(`Starting to make a coffee
+Grinding coffee beans
+Boiling water
+Mixing boiled water with crushed coffee beans
+Pouring coffee into the cup
+Pouring some milk into the cup
+Coffee is ready!`);
